817c09e1b3774fc17025692c367d937d0d96b24c
吴丹 <wudan@tuputech.com>
2017-01-10 20:59:04 +0800

    Initial Commit

---
+++ TUPUFaceDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TUPUFaceDemo
+//
+//  Created by drakeDan on 10/01/2017.
+//  Copyright © 2017 tupu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TUPUFaceDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TUPUFaceDemo
+//
+//  Created by drakeDan on 10/01/2017.
+//  Copyright © 2017 tupu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
