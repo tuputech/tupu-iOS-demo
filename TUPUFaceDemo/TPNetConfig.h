f57f070cf6170a9e6785e23459143c99d7f8d58c
吴丹 <wudan@tuputech.com>
2017-02-07 10:08:37 +0800

    change project settings

--- TUPUFaceDemo/TPNetConfig.h
+++ TUPUFaceDemo/TPNetConfig.h
@@ -9,7 +9,7 @@
 #ifndef TPNetConfig_h
 #define TPNetConfig_h
 
-#define TP_LICENSE_API @"http://tuputech.com:8802/v1/app/auth"
+#define TP_LICENSE_API @"http://183.60.177.232:8585/v1/app/auth"
 
 #define TP_APP_KEY @"f54b76527835ba62337c9e535d01590d"
 #define TP_APP_SECRET @"8ed03bf9f6687ae7344923898794277f25248d3c"
